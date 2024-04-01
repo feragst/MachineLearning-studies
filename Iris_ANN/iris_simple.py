f1a669587a8bb76864f64c3ff163aa513bb3caed
feragst <139084664+feragst@users.noreply.github.com>
2024-04-01 10:10:48 -0300

    Update iris_simple.py

--- Iris_ANN/iris_simple.py
+++ Iris_ANN/iris_simple.py
@@ -1,4 +1,6 @@
 import pandas as pd
+from keras.models import Sequential
+from keras.layers import Dense
 
 base = pd.read_csv('iriss.csv')
 previsores = base.iloc[:, 0:4].values
@@ -7,3 +9,10 @@ classe = base.iloc[:, 4].values
 from sklearn.model_selection import train_test_split
 
 previsores_treinamento, previsores_teste, classe_treinamento, classe_teste = train_test_split(previsores, classe, test_size=0.0825)
+
+classificador = Sequential()
+classificador.add(Dense(units = 4, activation= 'relu', input_dim = 4))
+classificador.add(Dense(units= 4, activation= 'relu'))
+classificador.add(Dense(units= 3, activation= 'softmax'))
+classificador.compile(optimizer = 'adam', loss= 'categorical_crossentropy', 
+                      metrics = ['categorical_accuracy'])
