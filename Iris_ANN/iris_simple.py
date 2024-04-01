cf7cb3cf1942b996370826a93f37e80dc14f67e3
feragst <139084664+feragst@users.noreply.github.com>
2024-04-01 15:50:12 -0300

    Update Iris_ANN

--- Iris_ANN/iris_simple.py
+++ Iris_ANN/iris_simple.py
@@ -6,6 +6,10 @@ base = pd.read_csv('iriss.csv')
 previsores = base.iloc[:, 0:4].values
 classe = base.iloc[:, 4].values
 
+from sklearn.preprocessing import LabelEncoder
+labelencoder = LabelEncoder()
+classe= labelencoder.fit_transform(classe)
+
 from sklearn.model_selection import train_test_split
 
 previsores_treinamento, previsores_teste, classe_treinamento, classe_teste = train_test_split(previsores, classe, test_size=0.0825)
@@ -16,3 +20,5 @@ classificador.add(Dense(units= 4, activation= 'relu'))
 classificador.add(Dense(units= 3, activation= 'softmax'))
 classificador.compile(optimizer = 'adam', loss= 'categorical_crossentropy', 
                       metrics = ['categorical_accuracy'])
+classificador.fit(previsores_treinamento, classe_treinamento, batch_size=10,
+                  epochs= 1000)
