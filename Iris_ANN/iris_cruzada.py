54fd07ba1c7a00e8c5751634f6fa7b096f32071c
feragst <139084664+feragst@users.noreply.github.com>
2024-04-03 11:58:41 -0300

    Create iris_cruzada

---
+++ Iris_ANN/iris_cruzada.py
@@ -0,0 +1,38 @@
+import pandas as pd
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.utils import to_categorical
+from scikeras.wrappers import KerasClassifier
+from sklearn.model_selection import cross_val_score
+
+base = pd.read_csv('iriss.csv')
+previsores = base.iloc[:, 0:4].values
+classe = base.iloc[:, 4].values
+
+from sklearn.preprocessing import LabelEncoder
+labelencoder = LabelEncoder()
+classe = labelencoder.fit_transform(classe)
+classe_dummy = to_categorical(classe, num_classes=3)
+
+def criarRede():
+    classificador = Sequential()
+    
+    classificador.add(Dense(units = 4, activation= 'relu', input_dim = 4))
+    classificador.add(Dense(units= 4, activation= 'relu'))
+    classificador.add(Dense(units= 3, activation= 'softmax'))
+    
+    from keras.optimizers import Adam
+    
+    otimizador = Adam(learning_rate= 0.001, clipvalue= 0.5)
+
+    
+    
+    classificador.compile(optimizer = otimizador, loss= 'categorical_crossentropy', 
+                         metrics = ['categorical_accuracy'])
+    return classificador
+
+classificador = KerasClassifier(build_fn=criarRede, epochs = 150, batch_size = 10)
+
+resultados = cross_val_score(estimator=classificador, X=previsores, y=classe, cv=10, scoring='accuracy')
+media = resultados.mean()
+desvio = resultados.std()
