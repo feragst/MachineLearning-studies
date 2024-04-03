add9d356d653d8a0b7a7a3ec006bb5bcd0010535
feragst <139084664+feragst@users.noreply.github.com>
2024-04-03 14:25:59 -0300

    Update iris_cruzada.py

--- Iris_ANN/iris_cruzada.py
+++ Iris_ANN/iris_cruzada.py
@@ -4,10 +4,17 @@ from keras.layers import Dense
 from keras.utils import to_categorical
 from scikeras.wrappers import KerasClassifier
 from sklearn.model_selection import cross_val_score
+import warnings
+warnings.filterwarnings("ignore")
+
 
 base = pd.read_csv('iriss.csv')
+
 previsores = base.iloc[:, 0:4].values
 classe = base.iloc[:, 4].values
+
+print("Dimensões dos previsores:", previsores.shape)
+print("Dimensões da classe:", classe.shape)
 
 from sklearn.preprocessing import LabelEncoder
 labelencoder = LabelEncoder()
@@ -21,18 +28,16 @@ def criarRede():
     classificador.add(Dense(units= 4, activation= 'relu'))
     classificador.add(Dense(units= 3, activation= 'softmax'))
     
-    from keras.optimizers import Adam
-    
-    otimizador = Adam(learning_rate= 0.001, clipvalue= 0.5)
 
     
-    
-    classificador.compile(optimizer = otimizador, loss= 'categorical_crossentropy', 
+    classificador.compile(optimizer = 'adam', loss= 'categorical_crossentropy', 
                          metrics = ['categorical_accuracy'])
     return classificador
 
 classificador = KerasClassifier(build_fn=criarRede, epochs = 150, batch_size = 10)
 
-resultados = cross_val_score(estimator=classificador, X=previsores, y=classe, cv=10, scoring='accuracy')
+resultados = cross_val_score(estimator=classificador, X=previsores, y=classe_dummy, 
+                             cv=10, scoring = 'accuracy')
+
 media = resultados.mean()
 desvio = resultados.std()
