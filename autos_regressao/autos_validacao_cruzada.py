096de8f60d11bd056bb5a989c50616bacac71bc0
feragst <139084664+feragst@users.noreply.github.com>
2024-04-16 11:59:30 -0300

    Create autos_validacao_cruzada

--- autos_regressao/autos_validacao_cruzada.py
+++ autos_regressao/autos_validacao_cruzada.py
@@ -1,6 +1,9 @@
 import pandas as pd
 from keras.models import Sequential
 from keras.layers import Dense
+from sklearn.model_selection import cross_val_score
+from scikeras.wrappers import KerasRegressor
+
 
 base = pd.read_csv('autos.csv', encoding = 'ISO-8859-1')
 
@@ -12,42 +15,21 @@ base = base.drop('dateCreated', axis=1)
 base = base.drop('nrOfPictures', axis=1)
 base = base.drop('postalCode', axis=1)
 base = base.drop('lastSeen', axis=1)
-
-base['name'].value_counts()
 base = base.drop('name', axis=1)
-base['seller'].value_counts()
 base = base.drop('seller', axis=1)
-base['offerType'].value_counts()
 base = base.drop('offerType', axis=1)
 
-#inconsistent1
-i1= base.loc[base.price < 10]
-base = base[base.price >10]
 
-#media de preços
-base.price.mean()
 
-#inconsistent2
-i2 = base.loc[base.price>350000]
+base = base.loc[base.price > 10]
 base = base[base.price < 350000]
-
-
-base.loc[pd.isnull(base['vehicleType'])]
-base['vehicleType'].value_counts() # limousine
-base.loc[pd.isnull(base['gearbox'])]
-base['gearbox'].value_counts() # manuell
-base.loc[pd.isnull(base['model'])]
-base['model'].value_counts() #golf
-base.loc[pd.isnull(base['fuelType'])]
-base['fuelType'].value_counts() #benzin
-base.loc[pd.isnull(base['notRepairedDamage'])]
-base['notRepairedDamage'].value_counts() #nein
 
 valores ={'vehicleType': 'limousine', 'gearbox': 'manuell', 'model': 'golf', 
           'fuelType': 'benzin',
           'notRepairedDamage': 'nein'}
 
 base = base.fillna(value = valores)
+
 previsores = base.iloc[:, 1:13].values
 preco_real = base.iloc[:, 0].values
 
@@ -68,17 +50,22 @@ categorical_columns = [0, 1, 3, 5, 8, 9, 10]
 column_transformer = ColumnTransformer([('one_hot_encoder', OneHotEncoder(), categorical_columns)], remainder='passthrough')
 previsores = column_transformer.fit_transform(previsores)
 
-#construcao da rede neural
+def criar_rede():
+    regressor = Sequential()
+    regressor.add(Dense(units = 158, activation = 'relu', input_dim = 316))
+    regressor.add(Dense(units = 158, activation = 'relu'))
+    regressor.add(Dense(units = 1,  activation = 'linear'))
+    regressor.compile(loss = 'mean_absolute_error', optimizer = 'Adam',
+                      metrics = ['mean_absolute_error'])
+    return regressor
 
-regressor = Sequential()
-regressor.add(Dense(units = 158, activation = 'relu', input_dim = 316))
-regressor.add(Dense(units = 158, activation = 'relu'))
-regressor.add(Dense(units = 1,  activation = 'linear'))
-regressor.compile(loss = 'mean_absolute_error', optimizer = 'Adam',
-                  metrics = ['mean_absolute_error'])
-regressor.fit(previsores, preco_real, batch_size = 300, epochs = 100)
+regressor = KerasRegressor(build_fn = criar_rede,
+                           epochs = 100,
+                           batch_size = 300)
 
-previsoes = regressor.predict(previsores)
-preco_real.mean()
-previsoes.mean()
+resultados = cross_val_score(estimator = regressor,
+                             X = previsores, y = preco_real,
+                             cv = 10, scoring = 'neg_mean_absolute_error')
 
+media = resultados.mean()
+desvio = resultados.std()
